150bc9bf651988edf10f40cdead5c3cf265e5bd2
trandoducnghia <trandoducnghia@gmail.com>
2020-06-07 21:15:42 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@
 module.exports = {
 	'url': 'mongodb://ds019829.mlab.com:19829',
 	'options': {
-		'user':   'ducnghia',
+		'user':   'heroku_gsm3z3qh',
 		'pass':   'ducnghia27',
-		'dbName': 'heroku_gsm3z3qh',
+		'dbName': 'ducnghia',
 		'useNewUrlParser': true,
 		'useUnifiedTopology': true,
 		//'autoIndex':       false,
