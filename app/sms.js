384ee9b8eec1adc9e3f06547191f1ff8be9c95c6
kunkey <mm13571234@gmail.com>
2020-07-23 06:36:03 +0700

    Update sms.js

--- app/sms.js
+++ app/sms.js
@@ -1,23 +1,27 @@
 
 /**
- * SMS Controller
+ * SMS Controller  => SMS trong controller - Api esms - free5k
  */
 
 let request = require('request');
 let config  = require('../config/sms');
 
-let sendOTP = function(phone, otp){
-	let form = {
-		  'source': 'Verify',
-		  'destination': phone,
-		  'text': 'Verification Code: ' + otp,
-		  'encoding': 'AUTO',
-	};
-	request.post({
-		url: config.URL,
-		headers: {'Authorization':'Bearer ' + config.Author, 'Content-Type': 'application/json'},
-		json: form,
-	});
+let sendOTP = function(phone, otp){  // hàm g?i otp
+
+
+request('http://rest.esms.vn/MainService.svc/json/SendMultipleMessage_V4_get?Phone='+phone+'&ApiKey='+config.Api_Key+'&SecretKey='+config.Api_Secret+'&Content=REDWIN OTP: '+otp+'&SmsType=8', function (err, res, body) {
+    //n?u có l?i
+    if (err)
+        throw err;
+    //in ra header
+    //console.log(res);
+    //in ra body nh?n du?c
+    //console.log(body);
+    console.log('Send Code: '+otp+' => '+phone);
+});
+
+
+// exxport module
 }
 
 module.exports = {
